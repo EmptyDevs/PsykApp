8225ba0d7c7500fe3cb619562a7b8ffdc95fbbcf
Maxime Colomes <maxime.colomes@epita.fr>
2020-01-21 18:57:16 +0100

    to merge

--- src/router/index.js
+++ src/router/index.js
@@ -8,14 +8,14 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
-  }
+  }//,
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   component: function () {
+  //     return import(/* webpackChunkName: "about" */ '../views/About.vue')
+  //   }
+  // }
 ]
 
 const router = new VueRouter({
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
-const GoogleFontsPlugin = require("google-fonts-webpack-plugin")
-
 module.exports = {
   "transpileDependencies": [
     "vuetify"
