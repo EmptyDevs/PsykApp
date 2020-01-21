b049c0b257632cccc7e2f53fc8c31692e73cf9af
Maxime Colomes <maxime.colomes@epita.fr>
2020-01-21 18:44:39 +0100

    font

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import 'material-design-icons/iconfont/material-icons.css'
+import 'typeface-roboto/index.css'
 
 Vue.config.productionTip = false
 
