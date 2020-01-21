b94aaf88d9d082089fc67a01f90b3685c1d17ff8
thomas.saison <thomas.saison@epita.fr>
2020-01-21 18:25:41 +0100

    push components

--- src/components/Appbar.vue
+++ src/components/Appbar.vue
@@ -1,18 +1,13 @@
 <template>
-  <v-app-bar color="deep-purple accent-4" dense dark>
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-    <v-toolbar-title>Page title</v-toolbar-title>
+  <v-app-bar 
+    color="#121040"
+    dark
+    dense
+  >
+    <v-toolbar-title>Psyk</v-toolbar-title>
 
     <v-spacer></v-spacer>
 
-    <v-btn icon>
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
-
-    <v-btn icon>
-      <v-icon>mdi-magnify</v-icon>
-    </v-btn>
 
     <v-menu left bottom>
       <template v-slot:activator="{ on }">
