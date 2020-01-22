94900d557c7c957170487c7c7dc376ef7eda6d4f
thomas.saison <thomas.saison@epita.fr>
2020-01-22 17:52:18 +0100

    psykapp change store

--- src/store/index.js
+++ src/store/index.js
@@ -6,17 +6,37 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     prduits: [
-      {id: 0, name:"Vodka", category_id: 1,  img:"assets/vodka.png"},
-
+      { id: 0, name: "Vodka", category_id: 1, img: "" },
+      { id: 1, name: "Pain", category_id: 2, img: "" },
+      { id: 2, name: "Ricard", category_id: 1, img: "" },
+      { id: 3, name: "Biere", category_id: 1, img: "" },
     ],
-    category : [
-      {id:1, name: "Drink"}
+    category: [
+      { id: 1, name: "Drink" },
+      { id: 2, name: "Food" },
+      { id: 3, name: "Service" },
+      { id: 4, name: "Smoke" },
+    ],
+    menu: [
+      {
+        category: "entree", list: [
+          { name: "ok" },
+          { name: "oko" },
+        ]
+      },
+      { category: "plat", list: [] },
+      { category: "dessert", list: [] },
     ]
   },
   mutations: {
   },
   getters: {
-    
+    stateMenu: state => {
+      return state.menu
+    },
+    stateCategory: state => {
+      return state.category
+    },
   },
   actions: {
   },
