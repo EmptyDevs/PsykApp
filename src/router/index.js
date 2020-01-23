0b94d1f21d88473361092c670acb48d6f32911e5
Maxime Colomes <maxime.colomes@epita.fr>
2020-01-23 01:22:44 +0100

    Changes on about page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../components/home_page'
-import Sos from '../components/sospage'
 import ShopPage from '../components/shoppage'
 import AboutPage from '../components/aboutpage'
 
@@ -13,11 +12,6 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
-  },
-  {
-    path: '/sos',
-    name: 'sospage',
-    component: Sos
   },
   {
     path: '/shop',
