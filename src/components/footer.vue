e99d4654c6d0d34ed636e3af0284613245a32b71
Maxime Colomes <maxime.colomes@epita.fr>
2020-01-24 21:31:11 +0100

    footer: phone added, still missing close button

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -7,9 +7,38 @@
 
       <div style="padding-left: 10px; padding-right: 10px"/>
 
-      <v-btn >
-        <v-icon>mdi-twitter</v-icon>
-      </v-btn>
+      <div class="text-center">
+        <v-dialog
+          v-model="dialog"
+          width="500"
+        >
+          <template v-slot:activator="{ on }">
+            <v-btn
+              v-on="on"
+            >
+              <v-icon>mdi-phone</v-icon>
+            </v-btn>
+          </template>
+
+          <v-card>
+            <v-card-title
+              class="headline grey lighten-2"
+              primary-title
+            >
+              Téléphone
+              <v-spacer></v-spacer>
+              <v-btn icon @click="dialog = false">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+            </v-card-title>
+            <br>
+            <v-card-text class="subtitle-1 text-center">
+              Si vous préférez entendre nos voix douces, voici notre numéro:<br>
+              <div style="padding:10px; padding-top:10px" class="title">+33 6 00 00 00 00</div>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
+      </div>
 
       <div style="padding-left: 10px; padding-right: 10px"/>
 
